f8d35503bdd9a06f31bceb9c7bb3dd0567e5f026
timmer98 <38386858+timmer98@users.noreply.github.com>
2022-05-23 20:19:58 +0200

    Added benchmarks for simple array initialization and initialization with a specific value to compare two different ways

---
+++ ArrayInitializationBenchmark/ArrayInitBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+namespace ArrayInitializationBenchmark
+{
+    [MemoryDiagnoser]
+    public class ArrayInitBenchmark
+    {
+        [Params(500_000, 1_000_000)]
+        public int ArraySize { get; set; }
+
+        [Benchmark]
+        public void OnlyInit()
+        {
+            bool[] array = new bool[ArraySize];
+        }
+
+        [Benchmark]
+        public void LoopInit()
+        {
+            bool[] array = new bool[ArraySize];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = true;
+            }
+        }
+
+        [Benchmark]
+        public void EnumerableRepeat()
+        {
+            var array = Enumerable.Repeat(true, ArraySize).ToArray();
+        }
+    }
+}
---
+++ ArrayInitializationBenchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using BenchmarkDotNet.Running;
+
+namespace ArrayInitializationBenchmark
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var summary = BenchmarkRunner.Run<ArrayInitBenchmark>();
+        }
+    }
+}
